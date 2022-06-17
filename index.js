7e5dee8de8d0637cffabd4b22ebe01b932f6855f
leah karienye <karienyeleah@gmail.com>
2022-06-18 01:21:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+const  createEmployeeRecord = function (names, digits){ 
+    const newEmployee = [ firstName = names,
+     familyName= names,
+     titleField = names,
+     payPerhour= digits,
+      field = names,
+  timeInEvents = [],
+ timeOutEvents = [] ]
+ 
+ }
+const createEmployeeRecords = function (array){
+ let newArray ; 
+ for (i= 0; i<newEmployee.length; i++){
+return newArray;
+}
+}
+
+const createTimeInEvent = function (type, hour, date){
+
+
+}
+
+const createTimeOutEvent = function (type, hour, date){
+
+}
+
+const hoursWorkedOnDate = function (){
+
+}
+
+const wagesEarnedOnDate = function (){
+
+}
+
+const findEmployeeByFirstName = function (srcArray, firstName){
+
+}
+
+const calculatePayroll = function (createEmployeeRecords){
+
+}
+
+
+
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    const payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) 
+
+    return payable 
+}
