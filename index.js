32a72fbc5b0b41117bf291d5579df99adea33a68
leah karienye <karienyeleah@gmail.com>
2022-06-18 20:51:02 +0300

    commit tree 7fb0cb36c62a45116af5bb8b255a08090f0f05c8

--- index.js
+++ index.js
@@ -16,33 +16,57 @@ return newArray;
 }
 }
 
-const createTimeInEvent = function (type, hour, date){
+const createTimeInEvent = function (timeIn){
+    let [date, hour] = timeIn.split(' ')
 
+    return timeInEvents.push ({
+        type: 'arrival',
+        hour: parseInt(hour),
+        date:'',
+    })
+  return this;
+}
+const createTimeOutEvent = function (timeOut){
+    let [date, hour] = timeOut.split(' ')
+
+    return timeOutEvents.push ({
+        type: 'arrival',
+        hour: parseInt(hour),
+        date:'',
+    })
+  return this;
 
 }
 
-const createTimeOutEvent = function (type, hour, date){
+const hoursWorkedOnDate = function (dateIn){
+    let workingHours = this.timeInEvents.find(function(e){
+        return e.date === dateIn;
+    })
+    let notWorkingHours = this.timeoutEvents.find(function(e){
+        return e.date === dateIn;
+    })
+    return (notWorkingHours.hour - workingHours.hour) / 100
 
 }
 
-const hoursWorkedOnDate = function (){
-
-}
-
-const wagesEarnedOnDate = function (){
+const wagesEarnedOnDate = function (pay){ 
+    let wages = hoursWorkedOnDate.call(this, pay)
+     this.payPerhour 
+     return parseFloat(wages.toString())
 
 }
 
 const findEmployeeByFirstName = function (srcArray, firstName){
-
+    return srcArray.find(function(rec){
+        return rec.firstName === firstName
+    })
 }
 
 const calculatePayroll = function (createEmployeeRecords){
-
+    return createEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    })
 }
-
-
-
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
